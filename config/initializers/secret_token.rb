f89472f63ebb6c3e795f86c686c1627b778a24f7
Willfire19 <willfire19@gmail.com>
2013-10-14 01:26:34 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DreamShare::Application.config.secret_token = 'ac262e793205ff46ce6b7f84e59646b9a85acaf1c86428f7f1ff6f08aa6d4cd49b0359400d8756977316de11466cb82a04a8e76914babb4c2578885f4a7ff714'
