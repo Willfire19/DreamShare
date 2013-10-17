a1b869fe892b693f8a7b92ce04ac208e78c783b3
Willfire19 <willfire19@gmail.com>
2013-10-17 02:12:16 -0400

    DreamShare now responsive. Background Pictures are Randomized.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+	def random_back_img
+		images = ["assets/Cloud1.JPG", "assets/Water.JPG", "assets/Ocean3.JPG", "assets/SunBoat2.JPG"]
+		images[rand(images.size)]
+	end
+
 end
